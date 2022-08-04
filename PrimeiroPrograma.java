7ac49d4023ea5d3e4fb062c9caa546a423ddfb9c
Lucas Diogo <lucasdiogo95@hotmail.com>
2022-08-04 20:40:41 -0300

    first commit

---
+++ PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package com.dio;
+
+import com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+  
+		Gato gato = new Gato(null, null, null);
+		 
+		System.out.println(gato);
+	}
+
+}
